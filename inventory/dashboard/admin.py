caabdfe6139a68e63835b91437ac253a5a6cf7e7
Burhanuddin Kader <burhankader5252@gmail.com>
2022-12-13 01:08:51 +0530

    models

--- inventory/dashboard/admin.py
+++ inventory/dashboard/admin.py
@@ -7,5 +7,8 @@ admin.site.site_header="BKaderInventory"
 class ProductAdmin(admin.ModelAdmin):
     list_display=('name', 'category','quantity')
     list_filter=['category']
+
+    
 admin.site.register(Product,ProductAdmin)
+admin.site.register(Order)
 # admin.site.unregister(Group)
